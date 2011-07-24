664dd2ad177632c60d89660a39027ec69849787e
Marc Boivin <boivin@ma14.com>
2011-07-24 11:38:47 -0400

    Better regex

--- themes/zap_quebec_page_portail/functions.php
+++ themes/zap_quebec_page_portail/functions.php
@@ -3,7 +3,7 @@
 add_filter('option_blogname', 'zap_filter_ZAP', 10, 1);
 
 function zap_filter_ZAP($title){
-	$title = preg_replace('/^ZAP-?/g', '', $title);
+	$title = preg_replace('/^ZAP( -|-|—| —)?/g', '', $title);
 	
 	return $title;
 }
