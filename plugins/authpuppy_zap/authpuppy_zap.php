70f023ed18c0be63727ba7821fd67d914c6c6334
Marc Boivin <boivin@ma14.com>
2011-07-24 11:16:53 -0400

    Debug mode

--- plugins/authpuppy_zap/authpuppy_zap.php
+++ plugins/authpuppy_zap/authpuppy_zap.php
@@ -58,7 +58,7 @@ class AuthPuppyNode
 	function online_users(){
 		$this->fetch_node_info();
 		print_r($this->node_info);
-		$nb = $this->node_info->NumOnlineUsers > 0 ? $this->node_info->NumOnlineUsers : 0;
+		$nb = $this->node_info->NumOnlineUsers;
 		
 		return $nb;
 	}
