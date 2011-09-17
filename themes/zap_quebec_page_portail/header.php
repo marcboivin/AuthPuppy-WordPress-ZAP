cad77c057d941f10d0a1760bbd863097a03b828e
Marc Boivin <boivin@ma14.com>
2011-09-17 13:55:42 -0400

    Header style

--- themes/zap_quebec_page_portail/header.php
+++ themes/zap_quebec_page_portail/header.php
@@ -65,12 +65,13 @@
 <body <?php body_class(); ?>>
 	<div id="container">
     <header>
-		<h1><span>ZAP</span> <?php bloginfo('name'); ?></h1>
 		<div id="signature">
 			<a href="http://zapquebec.org" id="logo">Service offert par ZAP Québec</a>
 			<a href="http://www.facebook.com/pages/ZAP-Qu%C3%A9bec/114289998224" id="facebook">Notre page Facebok</a>
 			<a href="http://twitter.com/#!/zapquebec" id="twitter">Suivez nous sur Twitter</a>
 			<div id="online_users"><span><?php apz_connected_users(); ?></span> usagé(s) en ligne</div>
 		</div>
+		
+		<h1><span>ZAP</span> <?php bloginfo('name'); ?></h1>
     </header>
     <div id="main" role="main">
