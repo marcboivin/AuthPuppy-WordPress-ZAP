0430abf543a9d16f0847031e8bcbcda9dc51028d
Marc Boivin <boivin@ma14.com>
2011-09-17 13:28:09 -0400

    Content plugins and autoloading, first version

---
+++ plugins/ententes-contenus/quoi_faire_a_qc.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package AuthPuppy ZAP
+ * @subpackage Quoi faire à Québec
+ */
+/*
+Plugin Name: Quoi faire à Québec
+Plugin URI: http://zapquebec.org
+Description: Entente de contenu avec Quoi faire à Québec
+Version: 0.1.0
+Author: Marc boivin
+Author URI: http://sobremarc.com
+License: GPLv2 or later
+*/
+
+add_action('zap_before_content', 'qfaqc_content');
+
+function qfaqc_content(){
+	?>
+		<iframe class="qfaqc" src="http://www.quoifaireaquebec.com/feeds/todayZAP" width="100%" hiehgt="210">
+			<p>Vore fureteur ne supporte pas les iframes.</p>
+		</iframe>
+	<?php
+}
--- themes/zap_quebec_page_portail/functions.php
+++ themes/zap_quebec_page_portail/functions.php
@@ -2,6 +2,7 @@
 
 add_filter('option_blogname', 'zap_filter_ZAP', 10, 1);
 
+define('ZAP_DEFAULT_PLUGINS', 'ententes-contenus/quoi_faire_a_qc')
 /*
 	emove ZAP from a title string
  */
@@ -11,4 +12,20 @@ function zap_filter_ZAP($title){
 	$title = preg_replace('/^ZAP( -|-|—| —)?/i', '', $title);
 	
 	return $title;
+}
+
+/*
+ * Load activates plugins so that the default content is shown 
+ * on all portal pages
+ * 
+ *
+*/
+function zap_load_default_content($plugins){
+	$plugins = explode($plugin, ',');
+	foreach ($plugins as $plugin){
+		$plugin = trim($plugin);
+		
+		$plugin_path = ABSPATH . 'wp-content/plugins/{$plugin}.php';
+	  	activate_plugin($plugin_path);
+	}
 }
