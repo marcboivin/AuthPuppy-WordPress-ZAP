07d6a4351035ca3d4ecbc0561e51c54555d57c56
Marc Boivin <boivin@ma14.com>
2011-09-18 13:51:21 -0400

    Style improvement

--- themes/zap_quebec_page_portail/header.php
+++ themes/zap_quebec_page_portail/header.php
@@ -75,6 +75,6 @@
 	
 		<h1><span>ZAP</span> <?php bloginfo('name'); ?></h1>
 	
-		<div id="online_users"><span><?php apz_connected_users(); ?></span> usagé(s) en ligne</div>
+		<div id="online_users"><span><?php zap_connected_users(); ?></span> usagé(s) en ligne</div>
     </header>
     <div id="main" role="main">
